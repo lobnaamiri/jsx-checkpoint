73d0b31b520bd7c262ec5f8a082569b74e390008
lobnaamiri <amirilobna2015@gmail.com>
2019-08-27 13:29:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import photo from './img/src.jpg';
+
 import './App.css';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{border:'solid 1 black', maxWidth:'100vw'}}>
+
+      <h1 className={"title red"}>JSX Checkpoint</h1>
+
+
+      <img src={photo} alt={"image1"} className={"bg"}/>
+
+
+      <img src="/public.jpg" alt={"image2"} className={"bg"}/>
+      
     </div>
   );
 }
